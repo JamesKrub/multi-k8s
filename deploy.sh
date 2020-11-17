60bb187931ff46b03fef4e418aed11e66c0edc76
james <anonymous@gmail.com>
2020-11-17 18:12:30 +0700

    added deployment script

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,7 @@ docker push wanadjanan/multi-worker:$SHA
 
 kubectl apply -f k8s
 
+
 kubectl set image deployment/server-deployment server=wanadjanan/multi-server:$SHA
 kubectl set image deployment/client-deployment client=wanadjanan/multi-client:$SHA
 kubectl set image deployment/worker-deployment worker=wanadjanan/multi-worker:$SHA
